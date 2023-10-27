fbc72616bc9ce535047c81c294ecda24f4cc5cd8
Tianyu Chen <chentianyu@outlook.com>
2023-10-27 13:03:56 -0400

    import

---
+++ src/ConnectedComponents.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConnectedComponents {
+
+    /*
+     * TODO
+     */
+    public static <V> void
+    connected_components(Graph<V> G, Map<V, V> representative) {
+        // your code goes here
+    }
+
+
+}
